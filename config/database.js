5e484179c5b06aab81b404c4ab98ca8254982270
rajat bhasin <rajatbhasin.in@gmail.com>
2018-05-14 19:20:30 +0530

    mc
    mc

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://10.0.3.5/means'
+    localUrl: 'mongodb://23.100.16.149/means'
 };
